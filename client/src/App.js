b6c37286a7e122c96c30ec476a1c8c773ce47dfe
Tanya Kokonyan <t.j.angelova@gmail.com>
2023-05-28 15:40:55 +0300

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import {Route,Routes} from 'react-router-dom';
+
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <Header/>
+      <Routes>
+        <Route>
+          
+        </Route>
+      </Routes>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
