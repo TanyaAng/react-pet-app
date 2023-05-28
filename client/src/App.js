8c9315d6a17dfd3e2f4ce65a0fe932f4148f5cac
Tanya Kokonyan <t.j.angelova@gmail.com>
2023-05-28 16:40:01 +0300

    Added Routing

--- client/src/App.js
+++ client/src/App.js
@@ -3,15 +3,15 @@ import {Route,Routes} from 'react-router-dom';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import './App.css';
+import Categories from './components/Categories/Categories';
 
 function App() {
   return (
     <div className="container">
       <Header/>
       <Routes>
-        <Route>
-          
-        </Route>
+        <Route path="/" element={<Categories/>}/>
+        <Route path="categories/:category" element={<Categories/>}/>
       </Routes>
       <Footer/>
     </div>
